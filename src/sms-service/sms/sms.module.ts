8ecc32bb31c6810f48e1ce45f934f26edf5b2bb1
Vovarama1992 <vovayhh9988@gmail.com>
2024-12-11 09:18:59 +0300

    fix

--- src/sms-service/sms/sms.module.ts
+++ src/sms-service/sms/sms.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SmsService } from './sms.service';
+import { KafkaConsumerController } from 'src/shared-modules/kafka/kafka.consumer';
 
 @Module({
   providers: [SmsService],
+  controllers: [KafkaConsumerController],
   exports: [SmsService],
 })
 export class SmsModule {}
